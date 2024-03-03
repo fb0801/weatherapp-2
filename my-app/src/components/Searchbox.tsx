c7214f629beae17aed3dbbdbfcc91cd62812618a
fb0801 <fbhatti0801@gmail.com>
2024-03-03 15:31:18 +0000

    searchbox 2

--- my-app/src/components/Searchbox.tsx
+++ my-app/src/components/Searchbox.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { IoSearch } from "react-icons/io5";
 
-type Props = {}
+type Props = {
+    className?: string;
+    value: string
+    onChange: React.ChangeEventHandler<HTMLInputElement> | undefined
+    onSubmit: React.FormEventHandler<HTMLFormElement> | undefined
+}
 
-export default function Searchbox({}: Props) {
+export default function Searchbox(props: Props) {
   return (
-    <form className='flex relative items-center justify-center h-10'>
-        <input type='text' placeholder='search location' className='px-4 py-2 w-[230px] border 
+    <form onSubmit={props.onSubmit} className='flex relative items-center justify-center h-10'>
+        <input type='text' value={props.value} onChange={props.onChange}
+        placeholder='search location' className='px-4 py-2 w-[230px] border 
         border-gray-300 rounded-l-md focus:outline-none 
         focus:border-blue-500 h-full'/>
         <button className='px-4 py-[9px] bg-blue-500
