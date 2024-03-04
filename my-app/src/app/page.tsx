0b515b4387ac1db3eab6f91f8bdc15206811a01f
fb0801 <fbhatti0801@gmail.com>
2024-03-04 17:49:12 +0000

    current day components 8

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -10,6 +10,7 @@ import { parseISO } from "date-fns";
 import Container from "@/components/Container";
 import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
 import WeatherIcon from "@/components/WeatherIcon";
+import { getDayOrNightIcon } from "@/utils/getDayOrNightIcon";
 
 //https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56
 
@@ -134,7 +135,7 @@ if (isLoading) return (
                 <div key={i} className="flex flex-col justify-between gap-2 items-center text-xs font-semibold">
                   <p className="whitespace-nowrap">{format(parseISO(d.dt_txt), "h:mm a")}</p>
 
-                    <WeatherIcon iconName={d.weather[0].icon} />
+                    <WeatherIcon iconName={getDayOrNightIcon(d.weather[0].icon, d.dt_txt)} />
 
                   <p>{convertKelvinToCelsius( d?.main.temp ?? 0)}</p>
                 </div>
@@ -142,10 +143,24 @@ if (isLoading) return (
               </div>
           </Container>
           </div>
+          <div className="flex gap-4">
+          {}
+                  <Container className="w-fit justify-center flex-col px-4 items-center">
+
+                    <p className="capitalize text-center">{firstData?.weather[0].description}</p>
+                    <WeatherIcon iconName={getDayOrNightIcon(firstData?.weather[0].icon ?? "", 
+                    firstData?.dt_txt ?? "")} />
+
+                  </Container>
+                  <Container className="bg-yellow-300/80 px-6 gap-4 justify-between overflow-x-auto"></Container>
+          {}
+          </div>
         </section>
         
         {/*forcast*/}
-        <section></section>
+        <section className="flex w-full flex-col gap-4">
+          <p className="text-2xl">Forcecast (7 days)</p>
+        </section>
       </main>
     </div>
   );
